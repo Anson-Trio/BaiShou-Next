247d1dc3d7afed4c36b6b2d63b4e1d0164da7a14
TenkouAkatsuki <alice_kei@163.com>
2026-06-21 13:57:22 +0800

    chore(github): 添加 Bug/需求/创意 Issue 模板
    反馈链接改为 /issues/new，引导用户选择对应模板与标签。

--- packages/shared/src/constants/github.constants.ts
+++ packages/shared/src/constants/github.constants.ts
@@ -1,4 +1,5 @@
 /** Official BaiShou-Next GitHub repository */
 export const GITHUB_REPO_URL = 'https://github.com/Anson-Trio/BaiShou-Next'
 
-export const GITHUB_ISSUES_URL = 'https://github.com/Anson-Trio/BaiShou-Next/issues'
+/** Opens the issue template chooser (Bug / 需求建议 / 创意想法) */
+export const GITHUB_ISSUES_URL = 'https://github.com/Anson-Trio/BaiShou-Next/issues/new'
